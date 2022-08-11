63d949933eb3bf71a62561b5f544868149ac56cf
Weiran Liu <weiran.lwr@alibaba-inc.com>
2022-08-11 17:59:04 +0800

    实现理想ZlTriple生成协议。

--- mpc4j-s2pc-pcg/src/main/java/edu/alibaba/mpc4j/s2pc/pcg/mtg/zl/ZlMtgParty.java
+++ mpc4j-s2pc-pcg/src/main/java/edu/alibaba/mpc4j/s2pc/pcg/mtg/zl/ZlMtgParty.java
@@ -1,33 +1,33 @@
-package edu.alibaba.mpc4j.s2pc.pcg.mtg.z2;
+package edu.alibaba.mpc4j.s2pc.pcg.mtg.zl;
 
 import edu.alibaba.mpc4j.common.rpc.MpcAbortException;
 import edu.alibaba.mpc4j.common.rpc.pto.SecurePto;
 import edu.alibaba.mpc4j.common.rpc.pto.TwoPartyPto;
-import edu.alibaba.mpc4j.s2pc.pcg.mtg.z2.Z2MtgFactory.Z2MtgType;
 
 /**
- * 布尔三元组生成协议参与方接口。
+ * l比特三元组生成协议参与方接口。
  *
- * @author Sheng Hu, Weiran Liu
- * @date 2022/02/07
+ * @author Weiran Liu
+ * @date 2022/8/11
  */
-public interface Z2MtgParty extends TwoPartyPto, SecurePto {
+public interface ZlMtgParty extends TwoPartyPto, SecurePto {
     /**
      * 返回协议类型。
      *
      * @return 协议类型。
      */
     @Override
-    Z2MtgType getPtoType();
+    ZlMtgFactory.ZlMtgType getPtoType();
 
     /**
      * 初始化协议。
      *
+     * @param l           比特长度。
      * @param maxRoundNum 最大单轮数量。
      * @param updateNum   更新数量。
      * @throws MpcAbortException 如果协议异常中止。
      */
-    void init(int maxRoundNum, int updateNum) throws MpcAbortException;
+    void init(int l, int maxRoundNum, int updateNum) throws MpcAbortException;
 
     /**
      * 执行协议。
@@ -36,5 +36,5 @@ public interface Z2MtgParty extends TwoPartyPto, SecurePto {
      * @return 参与方输出。
      * @throws MpcAbortException 如果协议异常中止。
      */
-    Z2Triple generate(int num) throws MpcAbortException;
+    ZlTriple generate(int num) throws MpcAbortException;
 }
