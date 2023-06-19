47504b54ba79254d9232e9124dac5743bd2b276d
fengqing <jerry.pl@alibaba-inc.com>
2023-06-19 17:52:39 +0800

    minor

--- mpc4j-common-circuit/src/main/java/edu/alibaba/mpc4j/common/circuit/z2/sorter/AbstractSorter.java
+++ mpc4j-common-circuit/src/main/java/edu/alibaba/mpc4j/common/circuit/z2/sorter/AbstractSorter.java
@@ -12,6 +12,18 @@ import edu.alibaba.mpc4j.common.rpc.MpcAbortException;
  * @date 2023/6/12
  */
 public abstract class AbstractSorter extends AbstractZ2Circuit implements Sorter {
+    /**
+     * bit length of input.
+     */
+    protected int l;
+    /**
+     * num
+     */
+    protected int num;
+    /**
+     * num of elements to be sorted
+     */
+    protected int sortedNum;
 
     public AbstractSorter(Z2IntegerCircuit circuit) {
         super(circuit.getParty());
@@ -19,6 +31,9 @@ public abstract class AbstractSorter extends AbstractZ2Circuit implements Sorter
 
     @Override
     public void sort(MpcZ2Vector[][] xiArrays) throws MpcAbortException {
-        sort(xiArrays, party.createOnes(xiArrays[0][0].bitNum()));
+        this.sortedNum = xiArrays.length;
+        this.l = xiArrays[0].length;
+        this.num = xiArrays[0][0].getNum();
+        sort(xiArrays, party.createOnes(num);
     }
 }
