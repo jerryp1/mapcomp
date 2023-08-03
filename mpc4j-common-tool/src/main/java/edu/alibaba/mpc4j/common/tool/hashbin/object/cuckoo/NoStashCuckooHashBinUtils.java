27b5442d843d8a29ff2f3871f8480c97e52014ff
学酥 <weiran.lwr@alibaba-inc.com>
2023-08-03 09:23:07 +0800

    refine no-stash cuckoo hash parameters. Now the parameter is smooth.

---
+++ mpc4j-common-tool/src/main/java/edu/alibaba/mpc4j/common/tool/hashbin/object/cuckoo/NoStashCuckooHashBinUtils.java
@@ -0,0 +1,57 @@
+package edu.alibaba.mpc4j.common.tool.hashbin.object.cuckoo;
+
+import edu.alibaba.mpc4j.common.tool.MathPreconditions;
+
+/**
+ * no-stash cuckoo hash bin utilities. We use regression method to obtain the parameters. Although the regression results
+ * show that h = 4 and h = 5 can have small bin num, i.e.,
+ * <p>h = 3: ε = 16.021n^{-0.521} + 5.0 / n </p>
+ * <p>h = 4: ε = 11.498n^{-0.609} + 5.0 / n </p>
+ * <p>h = 5: ε = 8.4398n^{-0.568} + 5.0 / n </p>
+ * practical tests show that the estimated bin num is not correct.
+ * Therefore, we use the same parameters for all cases.
+ *
+ * @author Weiran Liu
+ * @date 2023/8/2
+ */
+public class NoStashCuckooHashBinUtils {
+    /**
+     * private constructor.
+     */
+    private NoStashCuckooHashBinUtils() {
+        // empty
+    }
+
+    /**
+     * Gets ε for small item size for 3 hashes.
+     *
+     * @param maxItemSize number of items.
+     * @return ε.
+     */
+    public static double getH3SmallItemSizeEpsilon(int maxItemSize) {
+        MathPreconditions.checkPositive("maxItemSize", maxItemSize);
+        return 16.021 * Math.pow(maxItemSize, -0.521) + 5.0 / maxItemSize;
+    }
+
+    /**
+     * Gets ε for small item size for 4 hashes.
+     *
+     * @param maxItemSize number of items.
+     * @return ε.
+     */
+    public static double getH4SmallItemSizeEpsilon(int maxItemSize) {
+        MathPreconditions.checkPositive("maxItemSize", maxItemSize);
+        return 16.021 * Math.pow(maxItemSize, -0.521) + 5.0 / maxItemSize;
+    }
+
+    /**
+     * Gets ε for small item size for 5 hashes.
+     *
+     * @param maxItemSize maxItemSize number of items.
+     * @return ε.
+     */
+    public static double getH5SmallItemSizeEpsilon(int maxItemSize) {
+        MathPreconditions.checkPositive("maxItemSize", maxItemSize);
+        return 16.021 * Math.pow(maxItemSize, -0.521) + 5.0 / maxItemSize;
+    }
+}
