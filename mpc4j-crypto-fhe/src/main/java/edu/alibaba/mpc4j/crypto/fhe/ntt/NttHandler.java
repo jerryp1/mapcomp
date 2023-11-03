c7c420cf322b6e6e495d77705874f07885d03f98
学酥 <weiran.lwr@alibaba-inc.com>
2023-11-03 18:13:38 +0800

    refactor(mpc4j-crypto-fhe): add comments in NttHandler.java.

--- mpc4j-crypto-fhe/src/main/java/edu/alibaba/mpc4j/crypto/fhe/ntt/NttHandler.java
+++ mpc4j-crypto-fhe/src/main/java/edu/alibaba/mpc4j/crypto/fhe/ntt/NttHandler.java
@@ -8,73 +8,93 @@ import edu.alibaba.mpc4j.crypto.fhe.zq.MultiplyUintModOperand;
  * template is specialized with integer modular arithmetic for DWT over integer quotient rings, and is used in
  * polynomial multiplications and BatchEncoder. It is also specialized with double-precision complex arithmetic for
  * DWT over the complex field, which is used in CKKSEncoder.
- *
  * <p>
  * The implementation is from https://github.com/microsoft/SEAL/blob/v4.0.0/native/src/seal/util/dwthandler.h
+ * </p>
+ * For details of fast NTT and INTT, see the following paper:
+ * <p>
+ * Longa, Patrick, and Michael Naehrig. Speeding up the number theoretic transform for faster ideal lattice-based
+ * cryptography. CANS 2016, pp. 124-139.
  * </p>
  *
  * @author Qixian Zhou
  * @date 2023/8/27
  */
 class NttHandler {
+    /**
+     * lazy modulo arithmetic operation for q
+     */
+    private final ModArithLazy arithmetic;
 
-    private ModArithLazy arithmetic;
-
-    public NttHandler() {
-    }
-
+    /**
+     * Creates an NTT handler.
+     *
+     * @param arithmetic lazy modulo arithmetic operation for q.
+     */
     public NttHandler(ModArithLazy arithmetic) {
         this.arithmetic = arithmetic;
     }
 
-    // ntt forward
+    /**
+     * NTT based on the Cooley-Tukey (CT) butterfly. See Algorithm 1 of the paper for more details.
+     *
+     * @param values A vector a = (a[0], a[1], ..., a[n − 1]) ∈ Z_n^q in standard ordering.
+     * @param logN   log(n) such that n = 2^k so n is a power of 2.
+     * @param roots  a precomputed table Ψ[0, ..., n) ∈ Z_n^q storing powers of ψ in bit-reversed order.
+     * @param scalar an optional scalar that is multiplied to all output values.
+     */
     public void transformToRev(long[] values, int logN, MultiplyUintModOperand[] roots, MultiplyUintModOperand scalar) {
         transformToRev(values, 0, logN, roots, scalar);
     }
 
     /**
-     * @param valuesArray
-     * @param startIndex  now we just handle the valuesArray[startIndex * N, (startIndex + 1) * N)
-     * @param logN
-     * @param roots
-     * @param scalar
+     * NTT based on the Cooley-Tukey (CT) butterfly. See Algorithm 1 of the paper for more details.
+     *
+     * @param valuesArray A vector a = (a[0 + startIndex], ..., a[n + startIndex − 1]) ∈ Z_n^q in standard ordering.
+     * @param startIndex  the start index.
+     * @param logN        log(n) such that n = 2^k so that n is a power of 2.
+     * @param roots       a precomputed table Ψ[0, ..., n) ∈ Z_n^q storing powers of ψ in bit-reversed order.
+     * @param scalar      an optional scalar that is multiplied to all output values.
      */
+    @SuppressWarnings("PointlessArithmeticExpression")
     public void transformToRev(long[] valuesArray, int startIndex, int logN, MultiplyUintModOperand[] roots, MultiplyUintModOperand scalar) {
-
+        // constant transform size
         int n = 1 << logN;
         // registers to hold temporary values
         MultiplyUintModOperand r;
         long u;
         long v;
-        // pointers for faster indexing
-//        long x;
-//        long y;
-        // variables for indexing
+        // variables for indexing, gap is t in algorithm 1. t = n, but in the first loop t = t / 2.
         int gap = n >>> 1;
         int m = 1;
+        // rootsIndex is m + i
         int rootsIndex = 0;
-
+        // for (m = 1; m < n; m = 2m) do
         for (; m < (n >>> 1); m <<= 1) {
             int offSet = 0;
             if (gap < 4) {
+                // for (i = 0; i < m; i++) do
                 for (int i = 0; i < m; i++) {
+                    // S = Ψ[m + i]
                     r = roots[++rootsIndex];
-//                    x = values[offSet];
-//                    y = values[offSet + gap];
+                    // for (j = j_1; j ≤ j_2; j++) do, here j_1 = offSet
                     for (int j = 0; j < gap; j++) {
+                        // U = a[j]
                         u = arithmetic.guard(valuesArray[startIndex + offSet + j]);
+                        // V = a[j + t] · S
                         v = arithmetic.mulRoot(valuesArray[startIndex + offSet + gap + j], r);
-                        // compute and update
+                        // a[j] = U + V mod q
                         valuesArray[startIndex + offSet + j] = arithmetic.add(u, v);
+                        // a[j + t] = U − V mod q
                         valuesArray[startIndex + offSet + gap + j] = arithmetic.sub(u, v);
                     }
+                    // offset is j_1 = 2 · i · t
                     offSet += (gap << 1);
                 }
             } else {
+                // the following is similar, except that we manually expand loops for speeding up the performance
                 for (int i = 0; i < m; i++) {
                     r = roots[++rootsIndex];
-//                    x = values[offSet];
-//                    y = values[offSet + gap];
                     for (int j = 0; j < gap; j += 4) {
                         u = arithmetic.guard(valuesArray[startIndex + offSet + j]);
                         v = arithmetic.mulRoot(valuesArray[startIndex + offSet + gap + j], r);
@@ -99,17 +119,17 @@ class NttHandler {
                     offSet += (gap << 1);
                 }
             }
+            // t = t / 2
             gap >>>= 1;
         }
 
+        // handle scalar
+        int valuesIndex = 0;
         if (scalar != null) {
-            int valuesIndex = 0;
             MultiplyUintModOperand scaledR;
-
             for (int i = 0; i < m; i++) {
                 r = roots[++rootsIndex];
                 scaledR = arithmetic.mulRootScalar(r, scalar);
-
                 u = arithmetic.mulScalar(arithmetic.guard(valuesArray[startIndex + 0 + valuesIndex]), scalar);
                 v = arithmetic.mulRoot(valuesArray[startIndex + 1 + valuesIndex], scaledR);
                 valuesArray[startIndex + 0 + valuesIndex] = arithmetic.add(u, v);
@@ -117,56 +137,74 @@ class NttHandler {
                 valuesIndex += 2;
             }
         } else {
-
-            int valuesIndex = 0;
             for (int i = 0; i < m; i++) {
                 r = roots[++rootsIndex];
                 u = arithmetic.guard(valuesArray[startIndex + 0 + valuesIndex]);
                 v = arithmetic.mulRoot(valuesArray[startIndex + 1 + valuesIndex], r);
-
                 valuesArray[startIndex + 0 + valuesIndex] = arithmetic.add(u, v);
                 valuesArray[startIndex + 1 + valuesIndex] = arithmetic.sub(u, v);
-
                 valuesIndex += 2;
             }
         }
     }
 
+    /**
+     * Function INTT based on the Gentleman-Sande (GS) butterfly.
+     *
+     * @param values A vector a = (a[0], a[1], ..., a[n − 1]) ∈ Z_n^q in bit-reversed ordering.
+     * @param logN   log(n) such that n = 2^k so n is a power of 2.
+     * @param roots  a precomputed table Ψ^{-1}[0, ..., n) ∈ Z_n^q storing powers of ψ in bit-reversed order of [0, -n).
+     * @param scalar optional scalar that is multiplied to all output values.
+     */
     public void transformFromRev(long[] values, int logN, MultiplyUintModOperand[] roots, MultiplyUintModOperand scalar) {
         transformFromRev(values, 0, logN, roots, scalar);
     }
 
+    /**
+     * Function INTT based on the Gentleman-Sande (GS) butterfly.
+     *
+     * @param valuesArray A vector a = (a[0], a[1], ..., a[n − 1]) ∈ Z_n^q in bit-reversed ordering.
+     * @param logN        log(n) such that n = 2^k so n is a power of 2.
+     * @param roots       a precomputed table Ψ^{-1}[0, ..., n) ∈ Z_n^q storing powers of ψ in bit-reversed order of [0, -n).
+     * @param scalar      optional scalar that is multiplied to all output values.
+     */
     public void transformFromRev(long[] valuesArray, int startIndex, int logN, MultiplyUintModOperand[] roots, MultiplyUintModOperand scalar) {
-
         // constant transform size
         int n = 1 << logN;
         // registers to hold temporary values
         MultiplyUintModOperand r;
         long u;
         long v;
-        // pointers for faster indexing
-//        long x;
-//        long y;
-        // variables for indexing
+        // variables for indexing, gap is t in algorithm 2.
         int gap = 1;
+        // m = n, but in the first loop, h = m / 2, so here h is m in Algorithm 2
         int m = n >>> 1;
+        // rootsIndex = (h + i) % n. The precomputed table Ψ^{-1}[0, ..., n) is well-organized to reduce comuptation.
         int rootsIndex = 0;
-
+        // for (m = n; m > 1; m = m/2) do
         for (; m > 1; m >>= 1) {
             int offset = 0;
             if (gap < 4) {
+                // for (i = 0; i < h; i++) do
                 for (int i = 0; i < m; i++) {
+                    // S = Ψ^{-1}[h + i]
                     r = roots[++rootsIndex];
+                    // for (j = j_1; j ≤ j_2; j++) do, here j_1 = offSet
                     for (int j = 0; j < gap; j++) {
+                        // U = a[j]
                         u = valuesArray[startIndex + offset + j];
+                        // V = a[j + t]
                         v = valuesArray[startIndex + offset + gap + j];
-
+                        // a[j] = U + V mod q
                         valuesArray[startIndex + offset + j] = arithmetic.guard(arithmetic.add(u, v));
+                        // a[j + t] = (U − V) · S mod q
                         valuesArray[startIndex + offset + gap + j] = arithmetic.mulRoot(arithmetic.sub(u, v), r);
                     }
+                    // j_1 = j_1 + 2t
                     offset += (gap << 1);
                 }
             } else {
+                // the following is similar, except that we manually expand loops for speeding up the performance
                 for (int i = 0; i < m; i++) {
                     r = roots[++rootsIndex];
                     for (int j = 0; j < gap; j += 4) {
@@ -200,7 +238,6 @@ class NttHandler {
         if (scalar != null) {
             r = roots[++rootsIndex];
             MultiplyUintModOperand scaledR = arithmetic.mulRootScalar(r, scalar);
-
             if (gap < 4) {
                 for (int j = 0; j < gap; j++) {
                     u = arithmetic.guard(valuesArray[startIndex + j]);
@@ -210,7 +247,6 @@ class NttHandler {
                 }
             } else {
                 for (int j = 0; j < gap; j += 4) {
-
                     u = arithmetic.guard(valuesArray[startIndex + j]);
                     v = valuesArray[startIndex + gap + j];
                     valuesArray[startIndex + j] = arithmetic.mulScalar(arithmetic.guard(arithmetic.add(u, v)), scalar);
@@ -268,5 +304,4 @@ class NttHandler {
             }
         }
     }
-
 }
