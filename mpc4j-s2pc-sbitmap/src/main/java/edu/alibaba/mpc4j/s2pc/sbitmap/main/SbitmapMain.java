d3aaacdba3e035a61148b40c0bd83e52370658f5
fengqing <jerry.pl@alibaba-inc.com>
2023-11-29 18:01:29 +0800

    feat: batch test

--- mpc4j-s2pc-sbitmap/src/main/java/edu/alibaba/mpc4j/s2pc/sbitmap/main/SbitmapMain.java
+++ mpc4j-s2pc-sbitmap/src/main/java/edu/alibaba/mpc4j/s2pc/sbitmap/main/SbitmapMain.java
@@ -4,7 +4,12 @@ import edu.alibaba.mpc4j.common.tool.utils.PropertiesUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 import java.util.Properties;
+import java.util.stream.Collectors;
 
 /**
  * Sbitmap main class.
@@ -19,13 +24,31 @@ public class SbitmapMain {
         PropertiesUtils.loadLog4jProperties();
         // read configuration.
         LOGGER.info("read config file");
-        Properties properties = PropertiesUtils.loadProperties(args[0]);
-        SbitmapStarter fullSecureProtocol = new SbitmapStarter(properties);
-        // data number
 
-        // 这里应该设置测试不同的数据量
-        fullSecureProtocol.init();
-        fullSecureProtocol.start();
+        List<String> files = listFilesForFolder(new File(args[0]));
+        files = files.stream().sorted().collect(Collectors.toList());
+        List<Properties> ps = files.stream().map(PropertiesUtils::loadProperties).collect(Collectors.toList());
+        SbitmapStarter fullSecureProtocol = new SbitmapStarter();
+        fullSecureProtocol.setProperties(ps.get(0));
+        fullSecureProtocol.initRpc();
+        for (Properties p : ps) {
+            fullSecureProtocol.setProperties(p);
+            fullSecureProtocol.init();
+            fullSecureProtocol.start();
+        }
+        fullSecureProtocol.stopRpc();
         System.exit(0);
+    }
+
+    public static List<String> listFilesForFolder(final File folder) {
+        List<String> fileNames = new ArrayList<>();
+        for (final File fileEntry : Objects.requireNonNull(folder.listFiles())) {
+            if (fileEntry.isDirectory()) {
+                listFilesForFolder(fileEntry);
+            } else {
+                fileNames.add(folder.getAbsolutePath() + "/" + fileEntry.getName());
+            }
+        }
+        return fileNames;
     }
 }
