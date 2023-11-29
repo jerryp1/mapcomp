a58f35234a994de45b48238d9caf0248dba98d61
学酥 <weiran.lwr@alibaba-inc.com>
2023-11-29 15:11:03 +0800

    feat(mpc4j-crypto-fhe): UniformRandomGenerator now supports generation with seed.

--- mpc4j-crypto-fhe/src/main/java/edu/alibaba/mpc4j/crypto/fhe/rand/ClippedNormalDistribution.java
+++ mpc4j-crypto-fhe/src/main/java/edu/alibaba/mpc4j/crypto/fhe/rand/ClippedNormalDistribution.java
@@ -1,31 +1,32 @@
 package edu.alibaba.mpc4j.crypto.fhe.rand;
 
 /**
+ * Clipped normal distribution.
  * <p>
  * The implementation is from https://github.com/microsoft/SEAL/blob/v4.0.0/native/src/seal/util/clipnormal.h
  * </p>
  *
- * @author Qixian Zhou
+ * @author Qixian Zhou, Weiran Liu
  * @date 2023/9/20
  */
 public class ClippedNormalDistribution {
-
-    // todo: 需要一个表示分布的对象吗？如果需要如何表示？math3 的不支持 SecureRandom, 一定需要 SecureRandom 吗？
-//    private SecureRandom normal;
-
-    private double mean;
-
-    private double standardDeviation;
-
-    private double maxDeviation;
-
+    /**
+     * mean μ
+     */
+    private final double mean;
+    /**
+     * standard deviation σ
+     */
+    private final double standardDeviation;
+    /**
+     * max deviation for clipping
+     */
+    private final double maxDeviation;
 
     public ClippedNormalDistribution(double mean, double standardDeviation, double maxDeviation) {
-
         if (standardDeviation < 0) {
             throw new IllegalArgumentException("standardDeviation must be >= 0");
         }
-
         if (maxDeviation < 0) {
             throw new IllegalArgumentException("maxDeviation must be >= 0");
         }
@@ -34,15 +35,14 @@ public class ClippedNormalDistribution {
         this.maxDeviation = maxDeviation;
     }
 
-    public static double sample(UniformRandomGenerator engine, ClippedNormalDistribution parm) {
-        return parm.sample(engine);
-    }
-
-
+    /**
+     * Samples a randomness in clipped normal distribution.
+     *
+     * @param engine uniform random generator engine.
+     * @return a randomness in clipped normal distribution.
+     */
     public double sample(UniformRandomGenerator engine) {
-
         while (true) {
-            // 满足特定 均值和方差的 高斯分布的 随机数
             double value = engine.secureRandom.nextGaussian() * standardDeviation + mean;
             double deviation = Math.abs(value - mean);
             if (deviation <= maxDeviation) {
@@ -50,5 +50,4 @@ public class ClippedNormalDistribution {
             }
         }
     }
-
 }
