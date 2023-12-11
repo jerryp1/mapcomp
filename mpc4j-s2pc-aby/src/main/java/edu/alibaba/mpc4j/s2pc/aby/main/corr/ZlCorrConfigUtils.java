b9478a5a7396c763bd016b94ac290ddb08b9c132
fengqing <jerry.pl@alibaba-inc.com>
2023-12-11 17:38:16 +0800

    refactor: minor

--- mpc4j-s2pc-aby/src/main/java/edu/alibaba/mpc4j/s2pc/aby/main/corr/ZlCorrConfigUtils.java
+++ mpc4j-s2pc-aby/src/main/java/edu/alibaba/mpc4j/s2pc/aby/main/corr/ZlCorrConfigUtils.java
@@ -1,5 +1,8 @@
 package edu.alibaba.mpc4j.s2pc.aby.main.corr;
 
+import edu.alibaba.mpc4j.common.tool.EnvType;
+import edu.alibaba.mpc4j.common.tool.galoisfield.zl.Zl;
+import edu.alibaba.mpc4j.common.tool.galoisfield.zl.ZlFactory;
 import edu.alibaba.mpc4j.common.tool.utils.PropertiesUtils;
 import edu.alibaba.mpc4j.s2pc.aby.operator.corr.zl.ZlCorrConfig;
 import edu.alibaba.mpc4j.s2pc.aby.operator.corr.zl.ZlCorrFactory.ZlCorrType;
@@ -30,11 +33,13 @@ public class ZlCorrConfigUtils {
         String zlCorrTypeString = PropertiesUtils.readString(properties, "pto_name");
         ZlCorrType zlCorrType = ZlCorrType.valueOf(zlCorrTypeString);
         boolean silent = PropertiesUtils.readBoolean(properties, "silent");
+        int l = PropertiesUtils.readInt(properties, "zl");
+        Zl zl = ZlFactory.createInstance(EnvType.STANDARD, l);
         switch (zlCorrType) {
             case GP23:
                 return new Gp23ZlCorrConfig.Builder(silent).build();
             case RRK20:
-                return new Rrk20ZlCorrConfig.Builder(silent).build();
+                return new Rrk20ZlCorrConfig.Builder(zl, silent).build();
             default:
                 throw new IllegalArgumentException("Invalid " + ZlCorrType.class.getSimpleName() + ": " + zlCorrType.name());
         }
